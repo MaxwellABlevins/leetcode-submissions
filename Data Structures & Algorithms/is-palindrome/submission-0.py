4066707ab116986e5e6f4b7213e18a200d9b5856
Maxwell Blevins <126877025+MaxwellABlevins@users.noreply.github.com>
2026-06-22 16:39:57 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,16 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).casefold()
+        
+        L = 0
+        R = len(s) - 1
+
+        while L < R:
+            if s[L] != s[R]:
+                return False
+            L += 1
+            R -= 1
+        return True
